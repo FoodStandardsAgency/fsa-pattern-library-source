21befa674e77f247b49f86258bf2b3cce6973090
Peter Berryman <peterb@deeson.co.uk>
2021-07-20 21:40:26 +0100

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y",
+    "@whitespace/storybook-addon-html"
+  ],
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
+    );
+
+
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    config.module.rules.push({
+      test: /\.twig$/,
+      use: ['twig-loader'],
+    });
+
+    // Return the altered config
+    return config;
+  },
+}
---
+++ stories/Button.stories.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import button from './button.html.twig';
+import './button.scss';
+import './button.js';
+
+export default {
+  title: 'Example/Button',
+  argTypes: {
+    label: { control: 'text' },
+    primary: { control: 'boolean' },
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select', options: ['small', 'medium', 'large'] },
+    },
+    onClick: { action: 'onClick' },
+  },
+  parameters: {
+    controls: {
+      disabled: false,
+    }
+  }
+};
+
+const Template = ({ label, ...args }) => { 
+  console.log(args);
+  return button(args);
+}
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  primary: false,
+  label: 'Buttonnn',
+};
+
+
+/*
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
+*/
