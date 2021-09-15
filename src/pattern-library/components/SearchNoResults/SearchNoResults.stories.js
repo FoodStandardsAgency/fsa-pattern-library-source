53fb2f970336b7a1df62f9cbb35372e44ba345e2
Roxana-deeson <roxanaf@deeson.co.uk>
2021-09-15 14:24:46 +0100

    FSAR-110-Search no results

--- src/pattern-library/components/SearchNoResults/SearchNoResults.stories.js
+++ src/pattern-library/components/SearchNoResults/SearchNoResults.stories.js
@@ -1,8 +1,5 @@
 import searchNoResults from './searchNoResults.html.twig';
 import './searchNoResults.scss';
-// import listingItemJs from './ListingItem.js';
-
-// listingItemJs();
 
 export default {
   title: 'Pattern Library/Components/Search No Results',
@@ -20,7 +17,7 @@ const Template = (args) => {
 
 export const Content = Template.bind({});
 Content.args = {
-  url: 'http://google.com',
-  url2: 'http://youtube.com',
+  removeFiltersLink: 'http://google.com',
+  foodHygieneRating: 'http://youtube.com'
 
 };
