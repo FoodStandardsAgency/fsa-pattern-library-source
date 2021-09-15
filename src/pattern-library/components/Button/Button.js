7aae9abfc4c932e0c68087c7219cfe5b384aead4
Yinuo Meng <yinuomeng@yinuos-mbp.home>
2021-09-15 13:54:17 +0100

    Stop event propagation and attach event listener to all buttons

--- src/pattern-library/components/Button/Button.js
+++ src/pattern-library/components/Button/Button.js
@@ -1,12 +1,14 @@
 import './button.scss';
 import './button.html.twig';
 export default function () {
-
-  window.addEventListener("DOMContentLoaded",function() {
-    const button = document.querySelector('.button--event');
-    if (!button) return;
-    button.addEventListener('click', () => {
-      button.textContent = "I was clicked";
+  window.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.button--event');
+    if (!buttons.length) return;
+    buttons.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        e.stopPropagation();
+        button.textContent = 'I was clicked';
+      });
     });
   });
 }
