e5274612e74603e29c5a09ad28f3a5f70dfa4f3a
Roxana-deeson <roxanaf@deeson.co.uk>
2021-09-20 17:37:10 +0100

    FSAR-12-Landing links

--- src/pattern-library/components/LandingLinks/LandingLinks.js
+++ src/pattern-library/components/LandingLinks/LandingLinks.js
@@ -4,7 +4,14 @@ import './landingLinks.html.twig';
 export default function () {
 
   window.addEventListener('DOMContentLoaded', function () {
-   console.log('hellooo')
+   const listItems = document.querySelectorAll('.landing-links__item');
+   if(!listItems) return;
+   listItems.forEach(item => {
+     item.addEventListener('click', ()=> {
+       const link = item.querySelector('.landing-links__link');
+       link.click();
+     })
+   })
   
   });
 }
