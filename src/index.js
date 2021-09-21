816f5ffcda2396471cb6b0524302d1151179e84d
Emily <emilyh@deeson.co.uk>
2021-09-21 15:07:58 +0100

    FSAR-44: Search hero - Add search bar component - Add search bar header component
    Signed-off-by: Emily <emilyh@deeson.co.uk>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './base.scss';
+
+import './pattern-library/components/SearchBar/searchBar';
 import buttonJs from './pattern-library/components/Button/Button';
 import listingItemJs from './pattern-library/components/ListingItem/ListingItem';
 
