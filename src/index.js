a1090c5b4f6f829b3c9d4f6f7008039e5fc2cd3c
Emily <emilyh@deeson.co.uk>
2021-09-21 15:08:40 +0100

    FSAR-44: Search hero - Remove import
    Signed-off-by: Emily <emilyh@deeson.co.uk>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './base.scss';
 
-import './pattern-library/components/SearchBar/searchBar';
 import buttonJs from './pattern-library/components/Button/Button';
 import listingItemJs from './pattern-library/components/ListingItem/ListingItem';
 
