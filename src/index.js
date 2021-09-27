eb3bf855376f310b7833e2d2b16a7bd561edcea2
Roxana-deeson <84016618+Roxana-deeson@users.noreply.github.com>
2021-09-27 14:02:41 +0100

    Feature/fsar 12 landing links (#13)
    * Create file structure landing links
    
    * FSAR-12-Landing links
    
    * FSAR-1: Style setup
    - Change text sizes on swatches and vertically align
    - Fix max-width on grid
    - Change container margins to 2rem on desktop
    - Margin:0 on typography titles
    - Update font sizes according to typographic scale
    - Use mobile fonts on iPad
    - Install and enable autoprefixer
    
    Signed-off-by: Emily <emilyh@deeson.co.uk>
    
    * FSAR-1: Style setup
    - Revert comment
    
    Signed-off-by: Emily <emilyh@deeson.co.uk>
    
    * Change svg
    
    * Remove grid
    
    * Add css-loader back to webpack
    
    Co-authored-by: Emily <emilyh@deeson.co.uk>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './base.scss';
 import './pattern-library/components/Button/Button';
 import listingItemJs from './pattern-library/components/ListingItem/ListingItem';
+import landingLinksJs from './pattern-library/components/LandingLinks/LandingLinks';
 
 listingItemJs();
+landingLinksJs();
