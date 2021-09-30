c50b92051917a3c46451b7993759699cb9493e46
Roxana-deeson <roxanaf@deeson.co.uk>
2021-09-30 09:53:43 +0100

    Make only title clickable and add # as a link

--- src/pattern-library/components/ListingItem/ListingItem.js
+++ src/pattern-library/components/ListingItem/ListingItem.js
@@ -4,15 +4,5 @@ import './listingItem.html.twig';
 export default function () {
 
   window.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.listing-item__content');
-    if (!cards) return;
 
-    cards.forEach(card => {
-      card.addEventListener('click', (event) => {
-        event.stopPropagation();
-        const link = card.querySelector('.listing-item__title a');
-        link.click();
-      })
-    })
-  });
-}
+})}
