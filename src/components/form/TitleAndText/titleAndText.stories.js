177ae11dc0415f41f1202ded77c33a7c8ef98993
Yinuo Meng <mengyn1999@gmail.com>
2021-10-13 11:16:18 +0100

    Add an h2 variation to storybook

--- src/components/form/TitleAndText/titleAndText.stories.js
+++ src/components/form/TitleAndText/titleAndText.stories.js
@@ -15,11 +15,20 @@ const Template = (args) => {
   return titleAndText(args);
 };
 
-export const TitleAndText = Template.bind({});
-TitleAndText.storyName = 'Title and Text';
-TitleAndText.args = {
+export const h1 = Template.bind({});
+h1.storyName = 'h1';
+h1.args = {
   title: 'Form title',
   description:
     'Create an account to get food and allergy alerts by email or text message. This is a free service. Before you can subscribe we need to verify your email address. Please enter a valid email address in the box below and click "Create account".',
   tag: 'h1',
+};
+
+export const h2 = Template.bind({});
+h2.storyName = 'h2';
+h2.args = {
+  title: 'Form title',
+  description:
+    'Create an account to get food and allergy alerts by email or text message. This is a free service. Before you can subscribe we need to verify your email address. Please enter a valid email address in the box below and click "Create account".',
+  tag: 'h2',
 };
