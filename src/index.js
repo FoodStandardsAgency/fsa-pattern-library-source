6a148e65dd3510627b8f85029d23fe5e140005e6
c-ancia <c-ancia@users.noreply.github.com>
2021-10-18 13:38:08 +0100

    Feature/fsar 2 breadcrumb (#58)
    * [FSAR-2] breadcrumb
    
    * [FSAR-2] cleanup and fixing Error box
    
    * [FSAR-2] Subscription buttons WIP
    
    * [FSAR-2] fixing path
    
    * [FSAR-2] final amends for the breadcrumb
    
    * [FSAR-2] Nowrap on right element & full width on ipad when no right element
    
    * [FSAR-2] addressing comments
    
    Co-authored-by: Carole Ancia <carolea@deeson.co.uk>

--- src/index.js
+++ src/index.js
@@ -10,15 +10,19 @@ import './components/search/SearchBarHeader/searchBarHeader';
 import './components/search/SearchNoResults/searchNoResults';
 import './components/search/Showing/showing';
 import './components/search/SortBy/sortBy';
-import './components/forms/ErrorBox/ErrorBox';
+import './components/form/ErrorBox/ErrorBox';
 import './components/form/TitleAndText/titleAndText';
 import './components/form/Checkbox/checkbox';
-import './layout/twoColumn/twoColumn';
-import './layout/formLayout/formLayout';
 import './components/landing/LandingLinks/LandingLinks';
 
+import './layout/twoColumn/twoColumn';
+import './layout/formLayout/formLayout';
+import './layout/fullWidth/fullWidth';
+
 import searchBar from "./components/search/SearchBar/searchBar";
+import breadcrumbJs from './components/general/Breadcrumb/breadcrumb';
 
 // Fixes css vars on legacy browsers (i.e. IE11)
 cssVars();
 searchBar();
+breadcrumbJs();
