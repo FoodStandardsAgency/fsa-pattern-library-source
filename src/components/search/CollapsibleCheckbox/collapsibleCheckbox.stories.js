179a61018dccddeb27ba5461fb3aef3782985a68
Yinuo Meng <mengyn1999@gmail.com>
2021-10-19 09:34:13 +0100

    Create stories

---
+++ src/components/search/CollapsibleCheckbox/collapsibleCheckbox.stories.js
@@ -0,0 +1,18 @@
+import collapsibleCheckbox from './collapsibleCheckbox.html.twig';
+
+export default {
+  title: 'Components/Search/Collapsible Checkbox',
+};
+
+const Template = (args) => collapsibleCheckbox(args);
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'topics',
+  description: '1 selected',
+  fields: [
+    { label: 'Alergy alert', id: 'alergy-alert' },
+    { label: 'News', id: 'news' },
+    { label: 'Consultation', id: 'consultation' },
+  ],
+};
