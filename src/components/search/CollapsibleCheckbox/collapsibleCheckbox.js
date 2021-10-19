4592845cabc83e1fe5d8609b8242de8c48392046
Yinuo Meng <mengyn1999@gmail.com>
2021-10-19 13:53:21 +0100

    Generate description on first load

--- src/components/search/CollapsibleCheckbox/collapsibleCheckbox.js
+++ src/components/search/CollapsibleCheckbox/collapsibleCheckbox.js
@@ -3,16 +3,29 @@ import './collapsibleCheckbox.scss';
 
 export default function () {
   window.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('.accordion__button');
-    buttons.forEach((button) => {
+    const accordions = document.querySelectorAll('.accordion');
+    accordions.forEach((accordion) => {
+      //Update description on first load
+      const checkboxes = accordion.querySelectorAll('.accordion__input');
+      const numChecked = Array.from(checkboxes).filter(
+        (checkbox) => checkbox.checked
+      ).length;
+
+      const description = `${numChecked} selected`;
+      const descriptionElement = accordion.querySelector(
+        '.accordion__description'
+      );
+      descriptionElement.textContent = description;
+
+      //Update description when checking checkboxes
+
+      //Toggle accordion
+      const button = accordion.querySelector('.accordion__button');
       function toggleAccordion(e) {
-        const title = e.target.dataset.accordion;
-        const fieldsClass = '.accordion__fields--' + title;
-        const fields = document.querySelector(fieldsClass);
+        const fields = accordion.querySelector('.accordion__fields');
         fields.classList.toggle('.accordion__fields--active');
         console.log('I run');
       }
-
       button.addEventListener('click', toggleAccordion);
     });
   });
