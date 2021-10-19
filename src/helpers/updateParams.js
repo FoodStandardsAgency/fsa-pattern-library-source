c94c2efaa4fd6e2040f3da3d0eef447158029701
Emily <emilyh@deeson.co.uk>
2021-10-19 15:12:02 +0100

    FSAR-140: React sorting and paging - Replace existing parameters

--- src/helpers/updateParams.js
+++ src/helpers/updateParams.js
@@ -1,6 +1,7 @@
 export default function (name, value) {
   const currUrl = new URL(window.location.href);
   let params = new URLSearchParams(currUrl.search);
+  params.delete(name);
   params.append(name, value);
   const query = params.toString();
   window.location.href = window.location.origin + '?' + query;
