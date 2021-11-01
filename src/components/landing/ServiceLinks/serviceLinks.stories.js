b63a00fcad1c736025413046abc6dbcb8184eb98
Yinuo Meng <mengyn1999@gmail.com>
2021-11-01 14:24:07 +0000

    Create stories

---
+++ src/components/landing/ServiceLinks/serviceLinks.stories.js
@@ -0,0 +1,37 @@
+import serviceLinks from './serviceLinks.html.twig';
+
+export default {
+  title: 'Components/Landing/Service Links',
+};
+
+const Template = (args) => serviceLinks(args);
+
+export const FourLinks = Template.bind({});
+FourLinks.args = {
+  title: 'This is the services title',
+  description:
+    'This is a description for the services section. Phasellus magna. Curabitur ullamcorper ultricies nisi.',
+  links: [
+    { label: 'Safer food, better business', href: '#' },
+    { label: 'Apply for a regulated product authorisation', href: '#' },
+    { label: 'Register a food business', href: '#' },
+    { label: 'Allergen guidance for businesses', hash: '#' },
+  ],
+};
+
+export const EightLinks = Template.bind({});
+EightLinks.args = {
+  title: 'This is the services title',
+  description:
+    'This is a description for the services section. Phasellus magna. Curabitur ullamcorper ultricies nisi.',
+  links: [
+    { label: 'Safer food, better business', href: '#' },
+    { label: 'Apply fo r a regulated product authorisation', href: '#' },
+    { label: 'Register a food business', href: '#' },
+    { label: 'Allergen guidance for businesses', hash: '#' },
+    { label: 'Safer food, better business', href: '#' },
+    { label: 'Apply for a regulated product authorisation', href: '#' },
+    { label: 'Register a food business', href: '#' },
+    { label: 'Allergen guidance for businesses', hash: '#' },
+  ],
+};
