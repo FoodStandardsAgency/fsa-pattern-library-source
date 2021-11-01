fd68de13cd61af888989d1c7a168d3b4121aa526
Carole Ancia <carolea@deeson.co.uk>
2021-11-01 15:37:08 +0000

    [FSAR-13] Reorganising folders & adding a few extra props to checkboxes

--- src/components/search/CollapsibleCheckbox/collapsibleCheckbox.stories.js
+++ src/components/search/CollapsibleCheckbox/collapsibleCheckbox.stories.js
@@ -18,11 +18,11 @@ export const Open = Template.bind({});
 Open.args = {
   title: 'Types of Content',
   fields: [
-    { label: 'Alergy alert', id: 'alergy-alert', results: 20 },
-    { label: 'News', id: 'news', results: 10 },
-    { label: 'Consultation', id: 'consultation', results: 15 },
-    { label: 'Business Guidance', id: 'business-guidance', results: 23 },
-    { label: 'Consumer Guidance', id: 'consumer-guidance', results: 3 },
+    { label: 'Allergy alert', id: 'allergy-alert', name: 'type[allergy-alert]', value: 'allergy-alert', results: 20 },
+    { label: 'News', id: 'news', name: 'type[news]', value: 'news', results: 10 },
+    { label: 'Consultation', id: 'consultation', name: 'type[consultation]', value: 'consultation', results: 15 },
+    { label: 'Business Guidance', id: 'business-guidance', name: 'type[business-guidance]', value: 'business-guidance', results: 23 },
+    { label: 'Consumer Guidance', id: 'consumer-guidance', name: 'type[consumer-guidance]', value: 'consumer-guidance', results: 3 },
   ],
   open: true,
   selected: 'selected',
@@ -39,17 +39,17 @@ Multiple.args = {
   ...Open.args,
   title_one: "Topics",
   fields_one: [
-    { label: 'Campylobacter', id: 'campylobacter', results: 82 },
-    { label: 'Food safety', id: 'food-safety', results: 22 },
-    { label: 'Listeria', id: 'listeria', results: 3 },
-    { label: 'Nutrition in Northern Ireland', id: 'northern-ireland-nutrition', results: 129 },
-    { label: 'Shellfish', id: 'shellfish', results: 10 },
+    { label: 'Campylobacter', id: 'campylobacter', name: 'topics[campylobacter]', value: 'campylobacter', results: 82 },
+    { label: 'Food safety', id: 'food-safety', name: 'topics[food-safety]', value: 'food-safety', results: 22 },
+    { label: 'Listeria', id: 'listeria', name: 'topics[listeria]', value: 'listeria', results: 3 },
+    { label: 'Nutrition in Northern Ireland', id: 'northern-ireland-nutrition', name: 'topics[northern-ireland-nutrition]', value: 'northern-ireland-nutrition', results: 129 },
+    { label: 'Shellfish', id: 'shellfish', name: 'topics[shellfish]', value: 'shellfish', results: 10 },
   ],
   title_two: "Nation",
   fields_two: [
-    { label: 'England', id: 'england', results: 100 },
-    { label: 'Northern Ireland', id: 'northern-ireland', results: 89 },
-    { label: 'Wales', id: 'wales', results: 78 },
+    { label: 'England', id: 'england', name: 'nation[england]', value: 'england', results: 100 },
+    { label: 'Northern Ireland', id: 'northern-ireland', name: 'nation[northern-ireland]', value: 'northern-ireland', results: 89 },
+    { label: 'Wales', id: 'wales', name: 'nation[wales]', value: 'wales', results: 78 },
   ]
 };
 Multiple.parameters = {
