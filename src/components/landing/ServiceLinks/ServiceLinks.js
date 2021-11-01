5452f3596114cd334b18878aec7006a12c7de7d9
Yinuo Meng <mengyn1999@gmail.com>
2021-11-01 14:04:02 +0000

    Create html and js

---
+++ src/components/landing/ServiceLinks/ServiceLinks.js
@@ -0,0 +1,3 @@
+//ANCHOR questions
+//Should title and description be optional? How would the overlay be added? How would this affect how I write the service link component?
+//What's the tag for heading?
