e9efd7a153f820855b6e4e987b027961d908802a
Yinuo Meng <mengyn1999@gmail.com>
2021-11-02 11:22:06 +0000

    Style service links

--- src/components/landing/ServiceLinks/ServiceLinks.js
+++ src/components/landing/ServiceLinks/ServiceLinks.js
@@ -3,4 +3,28 @@
 //What's the tag for heading?
 
 import './serviceLinks.html.twig';
-import './serviceLinks.scss'
+import './serviceLinks.scss';
+
+export default function () {
+  window.addEventListener('DOMContentLoaded', function () {
+    const serviceLinks = document.querySelectorAll('.service-links__card');
+    const numOfLinks = serviceLinks.length;
+    if (!numOfLinks) return;
+
+    serviceLinks.forEach((serviceLink) => {
+      if (numOfLinks === 1) {
+        //Only one link
+        serviceLinks.classList.add('service-links__card--single');
+      } else if (numOfLinks === 2) {
+        //2 links
+        serviceLinks.classList.add('service-links__card--two');
+      } else if (numOfLinks % 3 === 0 || numOfLinks === 5) {
+        //3, 5 or 6 links
+        serviceLinks.classList.add('service-links__card--three');
+      } else {
+        //4, 6, 7 and 8 links
+        serviceLinks.classList.add('service-links__card--many');
+      }
+    });
+  });
+}
--- src/components/landing/ServiceLinks/serviceLinks.stories.js
+++ src/components/landing/ServiceLinks/serviceLinks.stories.js
@@ -1,5 +1,8 @@
 import serviceLinks from './serviceLinks.html.twig';
 import './serviceLinks.scss';
+import serviceLinksJS from './serviceLinks';
+
+serviceLinksJS();
 
 export default {
   title: 'Components/Landing/Service Links',
@@ -19,6 +22,22 @@ FourLinks.args = {
     { label: 'Allergen guidance for businesses', hash: '#' },
   ],
 };
+
+export const SixLinks = Template.bind({});
+SixLinks.args = {
+  title: 'This is the services title',
+  description:
+    'This is a description for the services section. Phasellus magna. Curabitur ullamcorper ultricies nisi.',
+  links: [
+    { label: 'Safer food, better business', href: '#' },
+    { label: 'Apply for a regulated product authorisation', href: '#' },
+    { label: 'Register a food business', href: '#' },
+    { label: 'Allergen guidance for businesses', hash: '#' },
+    { label: 'Safer food, better business', href: '#' },
+    { label: 'Apply for a regulated product authorisation', href: '#' },
+  ],
+};
+
 
 export const EightLinks = Template.bind({});
 EightLinks.args = {
