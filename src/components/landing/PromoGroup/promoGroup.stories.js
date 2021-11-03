ce6378a3eed54986cd92251e40e977dffde73694
Yinuo Meng <mengyn1999@gmail.com>
2021-11-03 11:32:24 +0000

    Add background image to cards

--- src/components/landing/PromoGroup/promoGroup.stories.js
+++ src/components/landing/PromoGroup/promoGroup.stories.js
@@ -15,36 +15,48 @@ oneLink.args = {
       description:
         'What you can do to reduce the chances of your food being infected with listeria.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/NDawe_Bell_FSA_150320_324.jpg?itok=E9uzlVec',
     },
     {
       title: 'For businesses',
       description:
         'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/industry-kitchen-cooking_0.jpg?itok=uyO6myYB',
     },
     {
       title: 'Our risk analysis',
       description:
         'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/AS_FSA_011216_L1_276.jpg?itok=oVF8mrBO',
     },
     {
       title: 'Listeria',
       description:
         'What you can do to reduce the chances of your food being infected with listeria.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/NDawe_Bell_FSA_150320_324.jpg?itok=E9uzlVec',
     },
     {
       title: 'For businesses',
       description:
         'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/industry-kitchen-cooking_0.jpg?itok=uyO6myYB',
     },
     {
       title: 'Our risk analysis',
       description:
         'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
       promo_link: '#',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/AS_FSA_011216_L1_276.jpg?itok=oVF8mrBO',
     },
   ],
 };
@@ -55,6 +67,8 @@ multipleLinks.args = {
   cards: [
     {
       title: 'For consumers',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/NDawe_Bell_FSA_150320_324.jpg?itok=E9uzlVec',
       link_list: [
         { label: 'Food hygiene ratings scheme', href: '#' },
         { label: 'Report a food problem', href: '#' },
@@ -65,6 +79,8 @@ multipleLinks.args = {
     },
     {
       title: 'For businesses',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/industry-kitchen-cooking_0.jpg?itok=uyO6myYB',
       link_list: [
         { label: 'Food safety for food delivery', href: '#' },
         { label: 'Placing a regulated product on the market', href: '#' },
@@ -75,6 +91,8 @@ multipleLinks.args = {
     },
     {
       title: 'About us',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/AS_FSA_011216_L1_276.jpg?itok=oVF8mrBO',
       link_list: [
         { label: 'Who are we', href: '#' },
         { label: 'Our risk analysis', href: '#' },
@@ -85,6 +103,8 @@ multipleLinks.args = {
     },
     {
       title: 'For consumers',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/NDawe_Bell_FSA_150320_324.jpg?itok=E9uzlVec',
       link_list: [
         { label: 'Food hygiene ratings scheme', href: '#' },
         { label: 'Report a food problem', href: '#' },
@@ -95,6 +115,8 @@ multipleLinks.args = {
     },
     {
       title: 'For businesses',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/industry-kitchen-cooking_0.jpg?itok=uyO6myYB',
       link_list: [
         { label: 'Food safety for food delivery', href: '#' },
         { label: 'Placing a regulated product on the market', href: '#' },
@@ -105,6 +127,8 @@ multipleLinks.args = {
     },
     {
       title: 'About us',
+      image:
+        'https://www.food.gov.uk/sites/default/files/styles/promo_medium/public/media/image/AS_FSA_011216_L1_276.jpg?itok=oVF8mrBO',
       link_list: [
         { label: 'Who are we', href: '#' },
         { label: 'Our risk analysis', href: '#' },
