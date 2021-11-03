282ad5be1cddd882b48a1981c93cc03a666e062a
Yinuo Meng <mengyn1999@gmail.com>
2021-11-03 10:47:44 +0000

    Update twig structure and stories to make the component take multiple cards

--- src/components/landing/PromoGroup/promoGroup.stories.js
+++ src/components/landing/PromoGroup/promoGroup.stories.js
@@ -2,13 +2,116 @@ import promoGroup from './promoGroup.html.twig';
 
 export default {
   title: 'Components/Landing/PromoGroup',
-}
+};
 
-const Template = args => promoGroup(args);
+const Template = (args) => promoGroup(args);
 
 export const oneLink = Template.bind({});
 oneLink.args = {
-  title: 'For businesses',
-  description: 'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
-  promo_link: '#',
-}
+  title: 'Popular content',
+  cards: [
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+    {
+      title: 'For businesses',
+      description:
+        'Cras sagittis. Vivamus in erat ut urna cursus vestibulum. Nulla facilisi.',
+      promo_link: '#',
+    },
+  ],
+};
+
+export const multipleLinks = Template.bind({});
+multipleLinks.args = {
+  title: 'Popular content',
+  cards: [
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+    {
+      title: 'For businesses',
+      link_list: [
+        { label: 'Food safety for food delivery', href: '#' },
+        { label: 'Placing a regulated product on the market', href: '#' },
+        { label: 'Starting a food business', href: '#' },
+        { label: 'Safer food, better business', href: '#' },
+        { label: 'Allergen guidance', href: '#' },
+      ],
+    },
+  ],
+};
