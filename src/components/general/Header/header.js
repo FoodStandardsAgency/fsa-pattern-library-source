d9f97bdea6431c95d1cad0f2f7b5d3941019d362
Yinuo Meng <mengyn1999@gmail.com>
2021-11-04 17:07:15 +0000

    Use padding rather than margin for the gaps between cards

---
+++ src/components/general/Header/header.js
@@ -0,0 +1,4 @@
+//TODO
+//Is language part of this ticket?
+//What's in the scope of this ticket? Do I put empty event listeners  on buttons? Which ones are links and which ones are buttons?
+//How do I link the search?
