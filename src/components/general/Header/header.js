4a0b4926f0e98e96133e2da91cf3b1a1fd80c78e
Yinuo Meng <mengyn1999@gmail.com>
2021-11-05 10:44:45 +0000

    Remove header file

--- src/components/general/Header/header.js
+++
@@ -1,4 +0,0 @@
-//TODO
-//Is language part of this ticket?
-//What's in the scope of this ticket? Do I put empty event listeners  on buttons? Which ones are links and which ones are buttons?
-//How do I link the search?
