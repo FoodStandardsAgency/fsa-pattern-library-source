026b3ae85fdb0ad313a79bfbb7b7f7adadee804b
Carole Ancia <carolea@deeson.co.uk>
2021-11-05 18:40:06 +0000

    [FSAR-167] adding more data to the element to make it functional

--- src/components/search/FiltersSelected/FiltersSelected.stories.js
+++ src/components/search/FiltersSelected/FiltersSelected.stories.js
@@ -12,12 +12,30 @@ const Template = (args) => {
 export const FiltersSelected = Template.bind({});
 FiltersSelected.args = {
   filter_names: [
-    'Alergy Alert',
-    'News',
-    'Ecoli',
-    'England',
-    'Northern Ireland',
-    'Consumer',
+    {
+      'name': 'Allergy Alert',
+      'value': 'allergy-alert'
+    },
+    {
+      'name': 'News',
+      'value': 'news'
+    },
+    {
+      'name': 'Ecoli',
+      'value': 'ecoli'
+    },
+    {
+      'name': 'England',
+      'value': 'england'
+    },
+    {
+      'name': 'Northern Ireland',
+      'value': 'northern-ireland'
+    },
+    {
+      'name': 'Consumer Guidance',
+      'value': 'consumer-guidance'
+    },
   ],
   remove_filter: 'Remove filter',
   remove_all_filters: 'Remove all filters',
