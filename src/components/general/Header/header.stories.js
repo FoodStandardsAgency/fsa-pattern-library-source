553e146985c2e671e0a62b7a53ab0b5c49a956e2
Yinuo Meng <mengyn1999@gmail.com>
2021-11-05 14:55:16 +0000

    Create stories and update html

---
+++ src/components/general/Header/header.stories.js
@@ -0,0 +1,25 @@
+import header from './header.html.twig';
+
+export default {
+  title: 'Components/General/Header',
+}
+
+const Template = args => header(args);
+
+export const Header = Template.bind({});
+Header.args = {
+  english: true,
+  english_link: '#',
+  english_label: 'English',
+  welsh_link: '#',
+  welsh_label: 'Cymraeg',
+  logo_link: '#',
+  logo_label: 'Food Standards Agency - Frontpage',
+  contact_link: '#',
+  contact_label: 'Contact',
+  report_link: '#',
+  report_label: 'Report',
+  ratings_link: '#',
+  ratings_label: 'Food hygiene ratings',
+  expanded: 'false',
+}
