006073b2197f78ad8f65b59a9fd8bb698ae26a54
Carole Ancia <carolea@deeson.co.uk>
2021-11-08 10:42:45 +0000

    Reorganising imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import './base.scss';
 import 'url-polyfill';
 import cssVars from 'css-vars-ponyfill';  
 
+/* General Components */
 import './components/general/Breadcrumb/breadcrumb';
 import './components/general/Button/button';
+
+/* Search Components */
+import './components/search/CollapsibleCheckbox/collapsibleCheckbox';
 import './components/search/FiltersSelected/filtersSelected';
 import './components/search/ListingItem/listingItem';
 import './components/search/Pagination/pagination';
@@ -11,34 +15,38 @@ import './components/search/SearchBarHeader/searchBarHeader';
 import './components/search/SearchNoResults/searchNoResults';
 import './components/search/Showing/showing';
 import './components/search/SortBy/sortBy';
-import './components/search/CollapsibleCheckbox/collapsibleCheckbox';
-import './components/form/ErrorBox/errorBoxs';
-import './components/form/TitleAndText/titleAndText';
+
+/* Form Components */
 import './components/form/Checkbox/checkbox';
-import './components/form/Radio/radio';
-import './components/landing/LandingLinks/landingLinks';
-import './components/landing/ServiceLinks/serviceLinks';
+import './components/form/ErrorBox/errorBoxs';
+import './components/form/ExternalLink/externalLink';
 import './components/form/InputField/inputField';
 import './components/form/MessageBox/messageBox';
+import './components/form/MultistepFunction/multistepFunction';
+import './components/form/Radio/radio';
 import './components/form/Tabs/tabs';
-import './components/form/ExternalLink/externalLink';
+import './components/form/TitleAndText/titleAndText';
+
+/* Landing Components */
 import './components/landing/LandingLinks/landingLinks';
 import './components/landing/LargePromoGroup/largePromoGroup';
 import './components/landing/LatestTeaser/latestTeaser';
-import './layout/twoColumn/twoColumn';
+import './components/landing/ServiceLinks/serviceLinks';
+
+/* Layout Components */
 import './layout/formLayout/formLayout';
+import './layout/twoColumn/twoColumn';
 import './layout/fullWidth/fullWidth';
-import './components/form/MultistepFunction/multistepFunction';
 
-
-import searchBar from "./components/search/SearchBar/searchBar";
 import breadcrumbJs from './components/general/Breadcrumb/breadcrumb';
-import sortBy from "./components/search/SortBy/sortBy"
 import pagination from "./components/search/Pagination/pagination";
+import searchBar from "./components/search/SearchBar/searchBar";
+import sortBy from "./components/search/SortBy/sortBy";
 
 // Fixes css vars on legacy browsers (i.e. IE11)
 cssVars();
+
+breadcrumbJs();
+pagination();
 searchBar();
 sortBy();
-pagination();
-breadcrumbJs();
