33b7ffacb729f86d24f24773044d9ddcd3c9d97f
Carole Ancia <carolea@deeson.co.uk>
2021-11-08 10:44:45 +0000

    Indentation

--- src/components/form/ExternalLink/externalLink.stories.js
+++ src/components/form/ExternalLink/externalLink.stories.js
@@ -2,13 +2,13 @@ import externalLink from './externalLink.html.twig';
 import './externalLink.js';
 
 export default {
-    title: 'Components/Form/External Link',
-    argTypes: {
-      type: {
-        defaultValue: 'External link',
-      },
+  title: 'Components/Form/External Link',
+  argTypes: {
+    type: {
+      defaultValue: 'External link',
     },
-  };
+  },
+};
 
 const Template = (args) => {
   return externalLink(args);
