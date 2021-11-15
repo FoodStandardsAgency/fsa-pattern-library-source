55c7259960e6a7e59156694921185805b8231b78
Yinuo Meng <mengyn1999@gmail.com>
2021-11-15 14:48:36 +0000

    Create filters header twig and basic story

---
+++ src/components/search/FiltersHeader/filtersHeader.stories.js
@@ -0,0 +1,13 @@
+import filtersHeader from './filtersHeader.html.twig';
+
+export default {
+  title: 'Components/Search/Filters Header',
+}
+
+const Template = args => filtersHeader(args);
+
+export const FiltersHeader = Template.bind({});
+FiltersHeader.args = {
+  title: 'Filter your search',
+  link_text: 'Return to results',
+}
