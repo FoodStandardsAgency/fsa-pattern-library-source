42814bb7802f1a39b411cc11c2d2fa0f7e8d7a36
Yinuo Meng <mengyn1999@gmail.com>
2021-11-15 16:44:29 +0000

    Update headers component to be a parent component

--- src/components/search/FiltersHeader/filtersHeader.js
+++ src/components/search/FiltersHeader/filtersHeader.js
@@ -6,22 +6,5 @@ export default function () {
     const returnButton = document.querySelector('.filters-header__return');
     if (!returnButton) return;
 
-    function scrollToResults() {
-      let targetEl;
-      const firstResult = document.querySelector('.listing-item');
-      const noResults = document.querySelector('.search-no-results');
-
-      if (firstResult) {
-        //Scroll to first result
-        targetEl = firstResult;
-      } else if (noResults) {
-        //Or scroll to no result screen if no results are avaialble;
-        targetEl = noResults;
-      } else return;
-
-      targetEl.scrollIntoView({ behavior: 'smooth' });
-    }
-
-    returnButton.addEventListener('click', scrollToResults);
   });
 }
