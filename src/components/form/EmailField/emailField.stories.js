f1dcfe03d6228ee6dc219748c770e2bb9bd2695d
Yinuo Meng <mengyn1999@gmail.com>
2021-11-18 09:43:47 +0000

    Amends

--- src/components/form/EmailField/emailField.stories.js
+++ src/components/form/EmailField/emailField.stories.js
@@ -10,9 +10,9 @@ const Template = (args) => emailField(args);
 export const EmailField = Template.bind({});
 EmailField.args = {
   title: 'Personal information',
-  email_label: 'email address',
+  email_label: 'Email address',
   email_link: '#',
-  email_address: 'rachaels@deeson.co.uk',
+  email_address: 'emails@company.co.uk',
   change_email_link: '#',
   change_email_label: 'Change your email address',
 };
