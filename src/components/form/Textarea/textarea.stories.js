9140ed1d96b38b51bc828361c5d0b49a662a4cdf
Yinuo Meng <mengyn1999@gmail.com>
2021-11-24 12:56:16 +0000

    Style component

--- src/components/form/Textarea/textarea.stories.js
+++ src/components/form/Textarea/textarea.stories.js
@@ -1,7 +1,12 @@
 import textarea from './textarea.html.twig';
+import './textarea.scss';
 
 export default {
   title: 'Components/Form/Textarea',
+  parameters: {
+    //Center the component so that the red error border-left is visible.
+    layout: 'centered',
+  },
 };
 
 const Template = (args) => textarea(args);
@@ -11,9 +16,9 @@ Normal.args = {
   error: false,
   name: 'crime',
   label: 'What is the food crime you are reporting?',
-  required: '',
+  required: 'required',
   description: 'Please describe the food crime that you are reporting',
-  rows: 20,
+  rows: 10,
 };
 
 export const Error = Template.bind({});
