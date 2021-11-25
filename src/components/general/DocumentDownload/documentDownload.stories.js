b66c00640e65f72b1483a430761504654d319bb6
Yinuo Meng <mengyn1999@gmail.com>
2021-11-25 11:19:05 +0000

    Add stories and improve twig

---
+++ src/components/general/DocumentDownload/documentDownload.stories.js
@@ -0,0 +1,24 @@
+import documentDownload from './documentDownload.html.twig';
+
+export default {
+  title: 'Components/General/Document Download',
+};
+
+const Template = (args) => documentDownload(args);
+
+export const WithoutRegion = Template.bind({});
+WithoutRegion.args = {
+  format: 'PDF',
+  link: '#',
+  view: 'View',
+  name: 'Step by step guide for meat approvals application as PDF',
+  as: 'as',
+  new_window: 'Open in a new window',
+  size: '559.42 KB',
+};
+
+export const WithRegion = Template.bind({});
+WithRegion.args = {
+  ...WithoutRegion.args,
+  region: 'England and Wales',
+};
