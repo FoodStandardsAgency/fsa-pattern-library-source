6a4e51d1029b873bf61137868920a892893cbc1c
Yinuo Meng <mengyn1999@gmail.com>
2021-12-07 11:11:01 +0000

    Add stories

---
+++ src/components/general/Feedback/feedback.stories.js
@@ -0,0 +1,23 @@
+import feedback from './feedback.html.twig';
+
+export default {
+  title: 'Components/General/Feedback',
+}
+
+export const Feedback = args => feedback(args);
+Feedback.args = {
+  section_summary: 'Feedback about the content on the page',
+  heading: 'Is this page useful',
+  yes: 'Yes',
+  useful: 'this page is useful',
+  or_text: 'or',
+  no: 'No',
+  not_useful: 'this page is not useful',
+  report_question: 'Is there anything wrong with this page?',
+  thank_you: 'Thanks for your feedback',
+  close: 'Close',
+  the_feedback_form: 'the feedback form',
+  improve_question: 'How should we improve this page?',
+  feedback: 'feedback',
+  submit: 'submit',
+}
