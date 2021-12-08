22dacf63ada7a414af3c99c99adc05a6d9b4305e
Yinuo Meng <mengyn1999@gmail.com>
2021-12-08 11:20:21 +0000

    Simplify the hiding classes and js

--- src/components/general/Feedback/feedback.js
+++ src/components/general/Feedback/feedback.js
@@ -12,44 +12,35 @@ export default function () {
     const feedback = document.querySelector('.feedback');
     if (!feedback) return;
 
-    const questions = feedback.querySelector('.feedback__questions');
-    const thank = feedback.querySelector('.feedback__thank');
-    const form = feedback.querySelector('.feedback__form-wrapper');
-
-    if (questions && thank && form) {
-      function showThanks(e) {
-        e.preventDefault();
-        questions.classList.add('feedback__questions--hide');
-        thank.classList.remove('feedback__thank--hide');
-        form.classList.add('feedback__form-wrapper--hide');
-      }
-
-      function showForm(e) {
-        e.preventDefault();
-        questions.classList.add('feedback__questions--hide');
-        thank.classList.add('feedback__thank--hide');
-        form.classList.remove('feedback__form-wrapper--hide');
-      }
-
-      function showQuestions(e) {
-        e.preventDefault();
-        questions.classList.remove('feedback__questions--hide');
-        thank.classList.add('feedback__thank--hide');
-        form.classList.add('feedback__form-wrapper--hide');
-      }
-
-      function addButtonListener(buttonSelector, listener) {
-        const button = feedback.querySelector(buttonSelector);
-        if (button) {
-          button.addEventListener('click', listener);
-        }
-      }
-
-      addButtonListener('.feedback__yes', showThanks);
-      addButtonListener('.feedback__no', showForm);
-      addButtonListener('.feedback__report-button', showForm);
-      addButtonListener('.feedback__close', showQuestions);
-      addButtonListener('.feedback__submit', showThanks);
+    function showThanks(e) {
+      e.preventDefault();
+      feedback.classList.add('feedback--thank');
+      feedback.classList.remove('feedback--form');
     }
+
+    function showForm(e) {
+      e.preventDefault();
+      feedback.classList.add('feedback--form');
+      feedback.classList.remove('feedback--thank');
+    }
+
+    function showQuestions(e) {
+      e.preventDefault();
+      feedback.classList.remove('feedback--form');
+      feedback.classList.remove('feedback--thank');
+    }
+
+    function addButtonListener(buttonSelector, listener) {
+      const button = feedback.querySelector(buttonSelector);
+      if (button) {
+        button.addEventListener('click', listener);
+      }
+    }
+
+    addButtonListener('.feedback__yes', showThanks);
+    addButtonListener('.feedback__no', showForm);
+    addButtonListener('.feedback__report-button', showForm);
+    addButtonListener('.feedback__close', showQuestions);
+    addButtonListener('.feedback__submit', showThanks);
   });
 }
