f41623ab00bb03c4819131d5db016bed1c294950
Yinuo Meng <mengyn1999@gmail.com>
2021-12-14 15:05:26 +0000

    Style component

--- src/components/form/PhoneField/phoneField.stories.js
+++ src/components/form/PhoneField/phoneField.stories.js
@@ -1,4 +1,5 @@
 import phoneField from './phoneField.html.twig';
+import './phoneField.scss';
 
 export default {
   title: 'Components/Form/Phone Field',
@@ -11,7 +12,7 @@ const Template = args => phoneField(args);
 
 export const Normal = Template.bind();
 Normal.args = {
-  title: 'Phone number',
+  label: 'Phone number',
   required: '',
   prefix: '+44',
   name: 'phone',
@@ -19,6 +20,7 @@ Normal.args = {
   error: false,
 }
 
+export const Error = Template.bind();
 Error.args = {
   ...Normal.args,
   required: 'required',
