3fd2cb1668a93a46097830eb85873ce196eded2c
Emily <emilyh@deeson.co.uk>
2021-12-16 11:43:43 +0000

    Local build - Remove extra echo

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -10,7 +10,6 @@ while true; do
   echo 'Finished building asset';
   rm -r ~/Sites/food.gov.uk/vendor/food-standards-agency/fsa-frontend/dist/;
   echo 'Copying dist to' ${DRUPAL_PATTERN_LIBRARY_URL};
-  echo ${DRUPAL_PATTERN_LIBRARY_URL}
   cp -r ~/Sites/fsa-pattern-library-source/dist ${DRUPAL_PATTERN_LIBRARY_URL}
   echo 'Finished copying dist';
   sleep 10
