61a7f69621c31bf4a9a0b0e2cd22e70dd3055f91
Yinuo Meng <mengyn1999@gmail.com>
2021-12-20 16:56:19 +0000

    Add for businesses stories

--- src/components/contact/HelpSecondaryMenu/helpSecondaryMenu.stories.js
+++ src/components/contact/HelpSecondaryMenu/helpSecondaryMenu.stories.js
@@ -8,8 +8,10 @@ export default {
   },
 };
 
-export const SecondaryMenu = (args) => helpSecondaryMenu(args);
-SecondaryMenu.args = {
+const Template = (args) => helpSecondaryMenu(args);
+
+export const ForConsumers = Template.bind({});
+ForConsumers.args = {
   heading: 'Help/contact menu',
   links: [
     {
@@ -57,6 +59,84 @@ SecondaryMenu.args = {
               href: '#',
               label: 'Report a food concern',
               active: false,
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find business services',
+              active: false,
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Give us feedback',
+              active: false,
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find contact details',
+              active: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export const ForBusinesses = Template.bind({});
+ForBusinesses.args = {
+  heading: 'Help/contact menu',
+  links: [
+    {
+      link: {
+        href: '#',
+        label: 'For consumers',
+        active: false,
+      },
+      submenu: {
+        links: [
+          {
+            link: {
+              href: '#',
+              label: 'Report a food problem',
+              active: false,
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Give us Feedback',
+              active: false,
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find contact details',
+              active: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      link: {
+        href: '#',
+        label: 'For businesses',
+        active: true,
+      },
+      submenu: {
+        links: [
+          {
+            link: {
+              href: '#',
+              label: 'Report a food concern',
+              active: true,
             },
           },
           {
