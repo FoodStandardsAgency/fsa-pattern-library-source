5d5673a7fc198225d83b1a1f4f10c9f2b7e6271e
Yinuo Meng <mengyn1999@gmail.com>
2021-12-20 14:51:49 +0000

    Create twig and storybook base to make data structure clear

---
+++ src/components/contact/HelpSecondaryMenu/helpSecondaryMenu.stories.js
@@ -0,0 +1,83 @@
+import helpSecondaryMenu from './helpSecondaryMenu.html.twig';
+
+export default {
+  title: 'Components/Help/Secondary Menu',
+};
+
+export const SecondaryMenu = (args) => helpSecondaryMenu(args);
+SecondaryMenu.args = {
+  heading: 'Help/contact menu',
+  links: [
+    {
+      link: {
+        href: '#',
+        label: 'For consumers',
+        active: 'true',
+      },
+      submenu: {
+        links: [
+          {
+            link: {
+              href: '#',
+              label: 'Report a food problem',
+              active: 'true',
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Give us Feedback',
+              active: 'false',
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find contact details',
+              active: 'false',
+            },
+          },
+        ],
+      },
+    },
+    {
+      link: {
+        href: '#',
+        label: 'For businesses',
+        active: 'false',
+      },
+      submenu: {
+        links: [
+          {
+            link: {
+              href: '#',
+              label: 'Report a food concern',
+              active: 'false',
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find business services',
+              active: 'false',
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Give us feedback',
+              active: 'false',
+            },
+          },
+          {
+            link: {
+              href: '#',
+              label: 'Find contact details',
+              active: 'false',
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
