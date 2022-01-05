7713ec18990f935830d96a0600b69b3bad8a4f9a
Yinuo Meng <mengyn1999@gmail.com>
2022-01-05 13:35:42 +0000

    Create story

---
+++ src/components/general/OnThisPage/onThisPage.stories.js
@@ -0,0 +1,19 @@
+import onThisPage from './onThisPage.html.twig';
+
+export default {
+  title: 'Components/General/On This Page',
+};
+
+export const OnThisPage = (args) => onThisPage(args);
+OnThisPage.args = {
+  on_this_page: 'on this page',
+  skip_this_menu: 'skip this menu',
+  links: [
+    { label: 'Explaning hazards', href: '#' },
+    { label: 'Food safety management procedures', href: '#' },
+    { label: 'HACCP system in meat plants', href: '#' },
+    { label: 'MyHACCP', href: '#' },
+    { label: 'MyHACCP step by step', href: '#' },
+    { label: 'Small business and retailers', href: '#' },
+  ],
+};
