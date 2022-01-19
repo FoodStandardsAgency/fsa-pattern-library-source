aa2f52b785c1bce115462a00bd2efc3a79d680e0
Emily <emilyh@deeson.co.uk>
2022-01-19 14:54:30 +0000

    FSAR-223: PDF and Print buttons - Styling - Storybook example - Add assets

--- src/components/article/PdfAndPrintButtons/pdfAndPrintButtons.stories.js
+++ src/components/article/PdfAndPrintButtons/pdfAndPrintButtons.stories.js
@@ -1,13 +1,19 @@
-import pdfAndPrintButtons from './documentDownload.html.twig';
+import pdfAndPrintButtons from './pdfAndPrintButtons.html.twig';
 import './pdfAndPrintButtons.scss';
 
 export default {
-  title: 'Components/Article/PDF and Print Buttons',
+  title: 'Components/Article/PDF And Print Buttons',
 };
 
-const Template = (args) => documentDownload(args);
+const Template = (args) => pdfAndPrintButtons(args);
 
 export const PDFAndPrintButtons = Template.bind({});
 PDFAndPrintButtons.args = {
-
+  pdf_link: '#',
+  pdf_name: 'A third of UK consumers are willing to try lab-grown meat and a quarter would try insects',
+  view: 'View',
+  as: 'as',
+  pdf: 'PDF',
+  new_window: 'Opens in a new window',
+  print_this_page: 'Print this page',
 };
