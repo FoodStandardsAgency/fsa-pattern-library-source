58cf5cf752bf31445d919d89178345feb07d7ff7
Emily <emilyh@deeson.co.uk>
2022-01-21 16:41:13 +0000

    FSAR-223: PDF and Print buttons - Add multipage guide functionality and examples

--- src/components/article/PdfAndPrintButtons/pdfAndPrintButtons.stories.js
+++ src/components/article/PdfAndPrintButtons/pdfAndPrintButtons.stories.js
@@ -9,6 +9,7 @@ const Template = (args) => pdfAndPrintButtons(args);
 
 export const PDFAndPrintButtons = Template.bind({});
 PDFAndPrintButtons.args = {
+  multipage_guide: false,
   pdf_link: '#',
   pdf_name: 'A third of UK consumers are willing to try lab-grown meat and a quarter would try insects',
   view: 'View',
@@ -16,4 +17,20 @@ PDFAndPrintButtons.args = {
   pdf: 'PDF',
   new_window: 'Opens in a new window',
   print_this_page: 'Print this page',
+};
+
+export const MultipageGuideVersion = Template.bind({});
+MultipageGuideVersion.args = {
+  multipage_guide: true,
+  pdf_link: '#',
+  pdf_name: 'A third of UK consumers are willing to try lab-grown meat and a quarter would try insects',
+  view: 'View',
+  as: 'as',
+  pdf: 'PDF',
+  new_window: 'Opens in a new window',
+  print_this_page: 'Print this page',
+  view_entire_guide: 'View entire guide as PDF',
+  print_guide: 'Print guide',
+  multipage_guide_pdf_link: '#',
+  multipage_guide_print_link: '#',
 };
