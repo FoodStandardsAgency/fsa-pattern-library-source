9b4473b0adae21c7bed231d9c3b1e3109dc7529c
Emily <emilyh@deeson.co.uk>
2022-01-21 16:24:36 +0000

    FSAR-35: Region variation block - Remove file value - Add examples of different html elements

--- src/components/article/RegionalVariationBlock/regionalVariationBlock.transparent.stories.js
+++ src/components/article/RegionalVariationBlock/regionalVariationBlock.transparent.stories.js
@@ -1,74 +1,101 @@
 import regionalVariationBlock from './regionalVariationBlock.html.twig';
 import './regionalVariationBlock.scss';
+import './../DocumentDownload/documentDownload.scss';
 
 export default {
   title: 'Components/Article/Regional Variation Block/Transparent',
 }
 
-export const WithoutDocumentDownload = args => regionalVariationBlock(args);
-WithoutDocumentDownload.args = {
+export const TextOnly = args => regionalVariationBlock(args);
+TextOnly.args = {
   region: ['England'],
-  wysiwyg_content: '<a href="#">Contact your local council in England</a> to apply for registration as a childminder if you provide food with your childminding business',
+  wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus tortor, pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>',
   transparent_background: true,
 }
 
-export const WithDocumentDownload = args => regionalVariationBlock(args);
-WithDocumentDownload.args = {
-  region: ['Wales'],
-  wysiwyg_content: '<a href="#">Contact your local council in England</a> to apply for registration as a childminder if you provide food with your childminding business',
-  transparent_background: true,
-  files: [
-    {
-      format: 'word',
-      link: '#',
-      view: 'View',
-      name: 'Step by step guide for meat approvals application',
-      as: 'as',
-      new_window: 'Open in a new window',
-      size: '559.42 KB',
-    },
-  ],
-}
-
-export const WithMultipleDocumentDownloads = args => regionalVariationBlock(args);
-WithMultipleDocumentDownloads.args = {
+export const WithLink = args => regionalVariationBlock(args);
+WithLink.args = {
   region: ['Northern Ireland'],
-  wysiwyg_content: '<a href="#">Contact your local council in England</a> to apply for registration as a childminder if you provide food with your childminding business',
+  wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>',
   transparent_background: true,
-  files: [
-    {
-      format: 'word',
-      link: '#',
-      view: 'View',
-      name: 'Step by step guide for meat approvals application',
-      as: 'as',
-      new_window: 'Open in a new window',
-      size: '559.42 KB',
-    },
-    {
-      format: 'pdf',
-      link: '#',
-      view: 'View',
-      name: 'Safer food, better business - Introduction',
-      as: 'as',
-      new_window: 'Open in a new window',
-      size: '1.09 MB',
-    },
-    {
-      format: 'jpeg',
-      link: '#',
-      view: 'View',
-      name: 'Safer food, better business - diary refill 4-weekly review',
-      as: 'as',
-      new_window: 'Open in a new window',
-      size: '858.14 KB',
-    },
-  ],
 }
 
-export const WithMultipleRegions = args => regionalVariationBlock(args);
-WithMultipleRegions.args = {
-  region: ['England','Wales', 'Northern Ireland'],
-  wysiwyg_content: '<a href="#">Contact your local council in England</a> to apply for registration as a childminder if you provide food with your childminding business',
+export const WithList = args => regionalVariationBlock(args);
+WithList.args = {
+  region: ['England', 'Northern Ireland'],
+  wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '<ul>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '</ul>\n' +
+      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '\n' +
+      '<ol>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '</ol>',
+  transparent_background: true,
+}
+
+export const WithFile = args => regionalVariationBlock(args);
+WithFile.args = {
+  region: ['Wales'],
+  wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '<section class="document-download">\n' +
+      '  <h3 class="document-download__region">England</h3>\n' +
+      '  <div class="document-download__content document-download__content--">\n' +
+      '    <span class="document-download__format document-download__format--" aria-hidden="true">PDF</span>\n' +
+      '    <div class="document-download__link-wrapper">\n' +
+      '      <a href="#" class="document-download__link" target="_blank"><span class="visually-hidden">View</span>Food Hygiene Advice for Small Businesses<span class="visually-hidden">as PDF(Opens in new window)</span></a>\n' +
+      '      <span class="document-download__size">12MB</span>\n' +
+      '    </div>\n' +
+      '  </div>\n' +
+      '</section>',
+  transparent_background: true,
+}
+
+export const Combination = args => regionalVariationBlock(args);
+Combination.args = {
+  region: ['England', 'Northern Ireland', 'Wales'],
+  wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '<section class="document-download">\n' +
+      '  <h3 class="document-download__region">England</h3>\n' +
+      '  <div class="document-download__content document-download__content--">\n' +
+      '    <span class="document-download__format document-download__format--" aria-hidden="true">PDF</span>\n' +
+      '    <div class="document-download__link-wrapper">\n' +
+      '      <a href="#" class="document-download__link" target="_blank"><span class="visually-hidden">View</span>Food Hygiene Advice for Small Businesses<span class="visually-hidden">as PDF(Opens in new window)</span></a>\n' +
+      '      <span class="document-download__size">12MB</span>\n' +
+      '    </div>\n' +
+      '  </div>\n' +
+      '</section>\n' +
+      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '<p>Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
+      '<section class="document-download">\n' +
+      '    <h3 class="document-download__region">Wales</h3>\n' +
+      '    <div class="document-download__content document-download__content--">\n' +
+      '        <span class="document-download__format document-download__format--" aria-hidden="true">WORD</span>\n' +
+      '        <div class="document-download__link-wrapper">\n' +
+      '            <a href="#" class="document-download__link" target="_blank"><span class="visually-hidden">View</span>Food Hygiene Advice for Small Businesses<span class="visually-hidden">as PDF(Opens in new window)</span></a>\n' +
+      '            <span class="document-download__size">180MB</span>\n' +
+      '        </div>\n' +
+      '    </div>\n' +
+      '</section>\n' +
+      '<ul>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '    <li>Lorem ipsum</li>\n' +
+      '</ul>\n' +
+      '<section class="document-download">\n' +
+      '    <h3 class="document-download__region">Northern Ireland</h3>\n' +
+      '    <div class="document-download__content document-download__content--">\n' +
+      '        <span class="document-download__format document-download__format--" aria-hidden="true">JPEG</span>\n' +
+      '        <div class="document-download__link-wrapper">\n' +
+      '            <a href="#" class="document-download__link" target="_blank"><span class="visually-hidden">View</span>Food Hygiene Advice for Small Businesses<span class="visually-hidden">as PDF(Opens in new window)</span></a>\n' +
+      '            <span class="document-download__size">20MB</span>\n' +
+      '        </div>\n' +
+      '    </div>\n' +
+      '</section>\n',
   transparent_background: true,
 }
