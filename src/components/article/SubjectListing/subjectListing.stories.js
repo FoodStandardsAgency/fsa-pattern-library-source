7b7c0a70f53e7b5bcdfe284c4a13118137afc021
Emily <emilyh@deeson.co.uk>
2022-02-01 15:12:05 +0000

    FSAR-17: Link list subject listing group - Revert, use paragraph in Drupal instead

--- src/components/article/SubjectListing/subjectListing.stories.js
+++ src/components/article/SubjectListing/subjectListing.stories.js
@@ -10,22 +10,67 @@ const Template = (args) => subjectListing(args);
 export const Basic = Template.bind({});
 Basic.args = {
   section_header: 'In this section',
-  title: 'Fish and fishery products',
-  links: [
-    { label: 'Live bivalve mollusc (LBM) and fishery product approvals', href: '#' },
-    { label: 'Importing fishery products or bivalve molluscs', href: '#' },
-    { label: 'Freezing requirements for fish and fishery products', href: '#' },
-  ],
+  subject_listing: [
+    {
+      title: 'Fish and fishery products',
+      links: [
+        { label: 'Live bivalve mollusc (LBM) and fishery product approvals', href: '#' },
+        { label: 'Importing fishery products or bivalve molluscs', href: '#' },
+        { label: 'Freezing requirements for fish and fishery products', href: '#' },
+      ],
+    },
+  ]
 };
 
 export const WithDescription = Template.bind({});
 WithDescription.args = {
   section_header: 'In this section',
-  title: 'Fish and fishery products',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed eros at sollicitudin. Nullam pharetra venenatis commodo. Proin mauris elit, convallis id nibh nec, laoreet venenatis enim.',
-  links: [
-    { label: 'Live bivalve mollusc (LBM) and fishery product approvals', href: '#' },
-    { label: 'Importing fishery products or bivalve molluscs', href: '#' },
-    { label: 'Freezing requirements for fish and fishery products', href: '#' },
-  ],
+  subject_listing: [
+    {
+      title: 'Fish and fishery products',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar sed eros at sollicitudin. Nullam pharetra venenatis commodo. Proin mauris elit, convallis id nibh nec, laoreet venenatis enim.',
+      links: [
+        { label: 'Live bivalve mollusc (LBM) and fishery product approvals', href: '#' },
+        { label: 'Importing fishery products or bivalve molluscs', href: '#' },
+        { label: 'Freezing requirements for fish and fishery products', href: '#' },
+      ],
+    },
+  ]
+};
+
+export const Multiple = Template.bind({});
+Multiple.args = {
+  section_header: 'In this section',
+  subject_listing: [
+    {
+      title: 'Fish and fishery products',
+      links: [
+        { label: 'Live bivalve mollusc (LBM) and fishery product approvals', href: '#' },
+        { label: 'Importing fishery products or bivalve molluscs', href: '#' },
+        { label: 'Freezing requirements for fish and fishery products', href: '#' },
+      ],
+    },
+    {
+      title: 'Shellfish guidance',
+      links: [
+        { label: 'Shellfish Official Controls', href: '#' },
+        { label: 'Classification', href: '#' },
+      ],
+    },
+    {
+      title: 'Monitoring, reports and surveys',
+      links: [
+        { label: 'Shellfish production area assessments', href: '#' },
+        { label: 'Chemical contaminant monitoring', href: '#' },
+        { label: 'Biotoxin and phytoplankton monitoring', href: '#' },
+        { label: 'Supplementary sampling', href: '#' },
+      ],
+    },
+    {
+      title: 'Related content',
+      links: [
+        { label: 'Prepare your business for Brexit', href: '#' },
+      ],
+    },
+  ]
 };
