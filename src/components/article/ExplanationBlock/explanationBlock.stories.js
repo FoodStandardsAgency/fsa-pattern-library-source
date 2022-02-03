4f5de806687d95a124c359bcbc2c3816ad7c0bbd
Emily <emilyh@deeson.co.uk>
2022-02-03 10:00:26 +0000

    FSAR-31: Explanation - Change names

--- src/components/article/ExplanationBlock/explanationBlock.stories.js
+++ src/components/article/ExplanationBlock/explanationBlock.stories.js
@@ -11,28 +11,28 @@ export default {
   },
 }
 
-export const WithoutTitle = args => explanationBlock(args);
-WithoutTitle.args = {
+export const Default = args => explanationBlock(args);
+Default.args = {
   type: 'general',
   wysiwyg_content: '<h4>Lorem ipsum</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus tortor, pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>',
 }
 
-export const WithTitle = args => explanationBlock(args);
-WithTitle.args = {
+export const Important = args => explanationBlock(args);
+Important.args = {
   type: 'important',
   heading: 'Important',
   wysiwyg_content: '<h4>Lorem ipsum</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus tortor, pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>',
 }
 
-export const WithLink = args => explanationBlock(args);
-WithLink.args = {
+export const FSAExplains = args => explanationBlock(args);
+FSAExplains.args = {
   type: 'fsa-explains',
   heading: 'FSA explains',
   wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. <a class="external-link" href="#">This is an external link</a> Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>',
 }
 
-export const WithList = args => explanationBlock(args);
-WithList.args = {
+export const BestPractice = args => explanationBlock(args);
+BestPractice.args = {
   type: 'best-practice',
   heading: 'Best practice',
   wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
@@ -50,8 +50,8 @@ WithList.args = {
     '</ol>',
 }
 
-export const WithFile = args => explanationBlock(args);
-WithFile.args = {
+export const Tips = args => explanationBlock(args);
+Tips.args = {
   type: 'tips',
   heading: 'Tips',
   wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
@@ -67,8 +67,8 @@ WithFile.args = {
     '</section>',
 }
 
-export const Combination = args => explanationBlock(args);
-Combination.args = {
+export const LegalAdvice = args => explanationBlock(args);
+LegalAdvice.args = {
   type: 'legal-advice',
   heading: 'Legal advice',
   wysiwyg_content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#">This is a link</a> pharetra tempor vehicula eu, sagittis nec odio. Suspendisse hendrerit nisi sed mi accumsan, sed tincidunt lacus volutpat.</p>\n' +
