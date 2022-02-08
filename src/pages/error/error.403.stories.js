1086b4ed880a81f6bc2c5ee8a4be21fe1e7fa736
Emily <emilyh@deeson.co.uk>
2022-02-08 10:56:43 +0000

    FSAR-62: Error pages - Clean up error pages

--- src/pages/error/error.403.stories.js
+++ src/pages/error/error.403.stories.js
@@ -1,6 +1,8 @@
 import error403 from './error403.html.twig';
 import error403WithArchive from './error403WithArchive.html.twig'
 
+import { social_svgs } from '../../components/svg-icons';
+
 export default {
   title: 'Pages/Error/Error 403',
   parameters: {
@@ -9,10 +11,30 @@ export default {
   },
 };
 
-export const Error403 = () => {
-  return error403();
-};
+const Template403 = (args) => {
+  return error403(args);
+}
 
-export const Error403WithArchive = () => {
+const Template403WithArchive = (args) => {
   return error403WithArchive();
-};
+}
+
+export const Error403 =  Template403.bind({});
+Error403.args = {
+  facebook: social_svgs.facebook,
+  twitter: social_svgs.twitter,
+  linkedin: social_svgs.linkedin,
+  youtube: social_svgs.youtube,
+  instagram: social_svgs.instagram,
+  blog: social_svgs.blog,
+}
+
+export const Error403WithArchive =  Template403WithArchive.bind({});
+Error403WithArchive.args = {
+  facebook: social_svgs.facebook,
+  twitter: social_svgs.twitter,
+  linkedin: social_svgs.linkedin,
+  youtube: social_svgs.youtube,
+  instagram: social_svgs.instagram,
+  blog: social_svgs.blog,
+}
--- src/pages/error/error.404.stories.js
+++ src/pages/error/error.404.stories.js
@@ -1,5 +1,7 @@
 import error404 from './error404.html.twig';
 
+import { social_svgs } from '../../components/svg-icons';
+
 export default {
   title: 'Pages/Error/Error 404',
   parameters: {
@@ -8,6 +10,17 @@ export default {
   },
 };
 
-export const Error404 = () => {
-  return error404();
-};
+const Template404 = (args) => {
+  return error404(args);
+}
+
+
+export const Error404 =  Template404.bind({});
+Error404.args = {
+  facebook: social_svgs.facebook,
+  twitter: social_svgs.twitter,
+  linkedin: social_svgs.linkedin,
+  youtube: social_svgs.youtube,
+  instagram: social_svgs.instagram,
+  blog: social_svgs.blog,
+}
