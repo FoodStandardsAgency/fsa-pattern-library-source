0fadf3ec3ab464386390ebde881bd468ab8a133b
Emily <emilyh@deeson.co.uk>
2022-02-09 10:56:04 +0000

    FSAR-22: - Fix arrow FSAR-32: - Use small font - Add hover state - Display external link icon FSAR-43: - Change so that multiple teams can be added FSAR-17: - Add margin top to description

--- src/components/article/TeamContacts/teamContacts.stories.js
+++ src/components/article/TeamContacts/teamContacts.stories.js
@@ -7,9 +7,32 @@ export default {
 
 const Template = (args) => teamContacts(args);
 
-export const RelatedContent = Template.bind({});
-RelatedContent.args = {
+export const TeamContacts = Template.bind({});
+TeamContacts.args = {
   team_contacts: 'Team contacts',
-  team_name: 'Food additives team ',
-  team_email: 'foodadditives@foos.gov.uk',
+  teams: [
+    {
+      team_name: 'Food additives team ',
+      team_email: 'foodadditives@foos.gov.uk',
+    },
+  ],
+};
+
+export const MultipleTeamContacts = Template.bind({});
+MultipleTeamContacts.args = {
+  team_contacts: 'Team contacts',
+  teams: [
+    {
+      team_name: 'Food additives team ',
+      team_email: 'foodadditives@food.gov.uk',
+    },
+    {
+      team_name: 'Information Knowledge Management',
+      team_email: 'informationmanagement@food.gov.uk',
+    },
+    {
+      team_name: 'Press and Media',
+      team_email: 'fsa.communications@food.gov.uk',
+    },
+  ],
 };
