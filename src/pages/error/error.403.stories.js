dc54a112eb8268d36f811b230486dd774db2c8cd
Emily <emilyh@deeson.co.uk>
2022-02-15 11:32:23 +0000

    FSAR-131: Campaign component - Default content so that we don't have the menu repeated in every file - Use svg-icons

--- src/pages/error/error.403.stories.js
+++ src/pages/error/error.403.stories.js
@@ -2,6 +2,7 @@ import error403 from './error403.html.twig';
 import error403WithArchive from './error403WithArchive.html.twig'
 
 import { social_svgs } from '../../components/svg-icons';
+import {navigation} from "@components/components/default-content";
 
 export default {
   title: 'Pages/Error/Error 403',
@@ -21,6 +22,7 @@ const Template403WithArchive = (args) => {
 
 export const Error403 =  Template403.bind({});
 Error403.args = {
+  navigation_content: navigation,
   facebook: social_svgs.facebook,
   twitter: social_svgs.twitter,
   linkedin: social_svgs.linkedin,
@@ -31,6 +33,7 @@ Error403.args = {
 
 export const Error403WithArchive =  Template403WithArchive.bind({});
 Error403WithArchive.args = {
+  navigation_content: navigation,
   facebook: social_svgs.facebook,
   twitter: social_svgs.twitter,
   linkedin: social_svgs.linkedin,
--- src/pages/landing/landing.campaign.stories.js
+++ src/pages/landing/landing.campaign.stories.js
@@ -1,4 +1,7 @@
 import landingCampaign from './landingCampaign.html.twig';
+import {social_svgs} from "@components/components/svg-icons";
+import {svgs} from "@components/components/svg-icons";
+import {navigation} from "@components/components/default-content";
 
 export default {
   title: 'Pages/Landing/Campaign',
@@ -10,7 +13,28 @@ export default {
   }
 };
 
-export const Campaign = ({ label, ...args }) => {
+const Template = (args) => {
   return landingCampaign(args);
 }
 
+
+export const Campaign =  Template.bind({});
+Campaign.args = {
+  navigation_content: navigation,
+  svg: {
+    import: svgs.import,
+    farming: svgs.farming,
+    regulated: svgs.regulated,
+    fish: svgs.fish,
+    meat: svgs.meat,
+    wine: svgs.wine,
+  },
+  social_svg: {
+    facebook: social_svgs.facebook,
+    twitter: social_svgs.twitter,
+    linkedin: social_svgs.linkedin,
+    youtube: social_svgs.youtube,
+    instagram: social_svgs.instagram,
+    blog: social_svgs.blog,
+  },
+}
