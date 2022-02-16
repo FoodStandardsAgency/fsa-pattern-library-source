302397244018b90e52a3744de37b07c6a20c87e2
Alexey Beloglazov <beloglazov91@gmail.com>
2022-02-16 17:33:23 +0300

    FSAR-45: New - call to action.

---
+++ src/components/search/RatingCta/ratingCta.stories.js
@@ -0,0 +1,8 @@
+import ratingCta from './ratingCta.html.twig';
+import './ratingCta.scss';
+
+export default {
+  title: 'Components/Search/Rating CTA',
+};
+
+const Template = (args) => ratingCta(args);
