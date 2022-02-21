904c1318b3fbc2a5cd569c7b478a86e8ed5eaf52
Emily <emilyh@deeson.co.uk>
2022-02-21 10:50:31 +0000

    FSAR-104: Landing text component - Clean up subject listing example

--- src/pages/landing/landing.subjectListing.stories.js
+++ src/pages/landing/landing.subjectListing.stories.js
@@ -1,4 +1,5 @@
-import landingSubjectListing from './landingSubjectListing.html.twig';
+import tableOfContents from "@components/components/article/TableOfContents/tableOfContents.html.twig";
+import subjectListing from './landingSubjectListing.html.twig';
 
 export default {
   title: 'Pages/Landing/Subject Listing',
@@ -8,6 +9,20 @@ export default {
   },
 };
 
-export const SubjectListing = () => {
-  return landingSubjectListing();
-};
+const Template = (args) => {
+  return subjectListing(args);
+}
+
+export const SubjectListing =  Template.bind({});
+SubjectListing.args = {
+  on_this_page:
+    tableOfContents({
+      title: 'on this page',
+      links: [
+        { label: 'Fish and fishery products', href: '#' },
+        { label: 'Shellfish guidance', href: '#' },
+        { label: 'Monitoring, reports and surveys', href: '#' },
+        { label: 'Related content', href: '#' },
+      ],
+    }),
+}
