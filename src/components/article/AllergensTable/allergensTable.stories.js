2ab50da723e0cef82d20d21b79514f4bdb77bb31
Emily <emilyh@deeson.co.uk>
2022-03-04 11:23:20 +0000

    FSAR-25: Tables - Update changelog - Add clarifying comment to the allergens table documentation

--- src/components/article/AllergensTable/allergensTable.stories.js
+++ src/components/article/AllergensTable/allergensTable.stories.js
@@ -3,6 +3,14 @@ import './allergensTable.scss';
 
 export default {
   title: 'Components/Article/Allergens Table',
+  parameters: {
+    docs: {
+      description: {
+        component: 'This table is specific to the alert content type and the product details component.' +
+        'For a more flexible table, see the example in the TextBlock component `Components/Article/Text Block`.',
+      },
+    },
+  }
 };
 
 export const AllergensTable = (args) => allergensTable(args);
