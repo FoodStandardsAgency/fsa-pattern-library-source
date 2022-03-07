05f35429d92fd8eb294f4d55def92046d6d0b710
Carole Ancia <carolea@deeson.co.uk>
2022-03-07 14:06:16 +0000

    [FSAR-285 & FSAR-249] Fixing layout and spacing

---
+++ src/pages/article/article.centered.stories.js
@@ -0,0 +1,17 @@
+import article from './articleCentered.html.twig';
+
+export default {
+  title: 'Pages/Article/Centered',
+  parameters: {
+    controls: { disable: true },
+    layout: 'fullscreen',
+  },
+};
+
+const Template = (args) => {
+  return article(args);
+};
+
+export const Centered = Template.bind({});
+Centered.args = {
+};
