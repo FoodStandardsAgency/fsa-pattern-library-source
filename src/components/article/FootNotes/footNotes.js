5570ea475d525ab6ea764b11d7e3fe3198ec77c7
Alexey Beloglazov <beloglazov91@gmail.com>
2022-03-08 15:52:08 +0300

    FHTML-7: Add footnotes.

--- src/components/article/FootNotes/footNotes.js
+++ src/components/article/FootNotes/footNotes.js
@@ -4,29 +4,29 @@ import './footNotes.scss';
 import smoothscroll from 'smoothscroll-polyfill';
 
 export default function () {
-    smoothscroll.polyfill();
+  smoothscroll.polyfill();
 
-    console.log('hey');
+  window.addEventListener('DOMContentLoaded', function () {
+    const notes = document.querySelectorAll(
+      '.foot-notes__link-list__item-link, .foot-notes__ref-link'
+    );
 
-    window.addEventListener('DOMContentLoaded', function () {
-        const notes = document.querySelectorAll('.foot-notes__link-list__item-link, .foot-notes__ref-link');
-        console.log(notes);
-        if (!notes.length) {
-            return;
+    if (!notes.length) {
+      return;
+    }
+
+    for (const note of notes) {
+      note.addEventListener('click', (e) => {
+        const id = e.target.getAttribute('href').substring(1);
+        const ref = document.querySelector(`[id='${id}']`);
+
+        if (ref) {
+          window.scroll({ top: ref.offsetTop, left: 0, behavior: 'smooth' });
+          window.location.hash = id;
         }
 
-        for (const note of notes) {
-            note.addEventListener('click', (e) => {
-                e.preventDefault();
-
-                const id = e.target.getAttribute('href').substring(1);
-                const ref = document.querySelector(`[id='${id}']`);
-
-                if (ref) {
-                    window.scroll({ top: ref.offsetTop, left: 0, behavior: 'smooth' });
-                    window.location.hash = id;
-                }
-            });
-        }
-    });
+        e.preventDefault();
+      });
+    }
+  });
 }
