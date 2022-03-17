d33ef215b25ce8ae254a84732cf82278370b1ec2
Ryan Blake <ryanb@deeson.com>
2022-03-17 12:55:14 +0000

    [FHTML-22] Add accessibility improvements.

--- src/components/article/RevisionLogBottom/revisionLogBottom.js
+++ src/components/article/RevisionLogBottom/revisionLogBottom.js
@@ -13,10 +13,20 @@ export default function () {
     const showButton = document.querySelector('.revision-log__toggle-button-show');
     const hideButton = document.querySelector('.revision-log__toggle-button-hide');
     const logsContainer = document.querySelector('.revision-log');
+    const logsList = document.querySelector('.revision-log__link-list');
     // Add eventlisteners for buttons.
     function toggleClass() {
       logsContainer.classList.toggle ('revision-log--open');
       logsContainer.classList.toggle ('revision-log--closed');
+      if (logsContainer.classList.contains('revision-log--open')) {
+         showButton.setAttribute("aria-hidden", "true");
+         hideButton.setAttribute("aria-hidden", "false"); 
+         logsList.setAttribute("aria-hidden", "false");
+      } else {
+         showButton.setAttribute("aria-hidden", "false"); 
+         hideButton.setAttribute("aria-hidden", "true"); 
+         logsList.setAttribute("aria-hidden", "true"); 
+      }
     }
     if (showButton) {
       showButton.addEventListener('click', toggleClass);
