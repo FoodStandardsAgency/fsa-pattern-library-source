380f62bda51d99ebcecd42a47b2641b780ac8dac
Yinuo Meng <mengyn1999@gmail.com>
2022-03-21 09:34:55 +0000

    [FFHRS-2] Set up storybook

---
+++ src/components/fhrs/LocalAuthority/localAuthority.stories.js
@@ -0,0 +1,18 @@
+import localAuthority from './localAuthority.html.twig';
+import logo from './site-logo-svg';
+
+export default {
+  title: 'Components/FHRS/Local Authority',
+};
+
+export const LocalAuthority = (args) => localAuthority(args);
+LocalAuthority.args = {
+  local_authority: 'Local Authority',
+  name_title: 'Name',
+  name_content: 'Coventry City Council',
+  website_title: 'Website',
+  website_url: 'www.coventry.gov.uk',
+  email_title: 'Email',
+  email_address: 'ehcommercial@coventry.gov.uk',
+  logo_svg: logo,
+};
