d092c23e67f46dbf5cacaa177680e714ab8d70c9
Alexey Beloglazov <beloglazov91@gmail.com>
2022-03-24 21:58:29 +0400

    HACCP-6: Progress bar.

--- src/components/article/WebToolProgressBar/webToolProgressBar.js
+++ src/components/article/WebToolProgressBar/webToolProgressBar.js
@@ -10,5 +10,25 @@ export default function () {
 
     DOMContentFirstLoad = false;
 
+    const labels = document.querySelectorAll('.webtool-progress-bar__label');
+
+    for (const label of labels) {
+      label.addEventListener('click', function(e) {
+        const parentLine = e.target.closest('.webtool-progress-bar__line');
+        const isOpened = parentLine.classList.contains('webtool-progress-bar__line-opened');
+
+        const lines = e.target.closest('.webtool-progress-bar').querySelectorAll('.webtool-progress-bar__line');
+
+        for (const line of lines) {
+          line.classList.remove('webtool-progress-bar__line-opened');
+        }
+
+        if (isOpened) {
+          return;
+        }
+
+        parentLine.classList.add('webtool-progress-bar__line-opened');
+      });
+    }
   });
 }
