ee7ca56e7344021a35c061cac0489436274c28fd
Yinuo Meng <78742598+Yinuo-M@users.noreply.github.com>
2022-03-24 15:18:18 +0000

    Feature/ffhrs 2 local authority (#332)
    * [FFHRS-2] Make external link open in a new window
    
    * [FFHRS-2] Create twig template for local authority
    
    * [FFHRS-2] Update twig
    
    * [FFHRS-2] Set up storybook
    
    * [FFHRS-2] Export twig and scss and import in index.js
    
    * [FFHRS-2] Format index.js according to linting feedback
    
    * [FFHRS-2] Style local authority component
    
    * [FFHRS-2] Create FHRS layout
    
    * [FFHRS-2] Rename coventry logo svg
    
    * [FFHRS-2] Create FHRS page
    
    * [FFHRS-2] Import fhrsLayout in index.js
    
    * [FFHRS-2] Lint
    
    * [FFHRS-2] Remove unnecessary margin
    
    * [FFHRS-2] Update changelog
    
    * [FFHRS-2] Update svg css to work in IE11
    
    * [FFHRS-2] Update changelog
    
    * [FFHRS-2] Update changelog
    
    * [FFHRS-2] Update changelog

---
+++ src/components/fhrs/LocalAuthority/localAuthority.stories.js
@@ -0,0 +1,19 @@
+import localAuthority from './localAuthority.html.twig';
+import './localAuthority.scss';
+import logo from './coventryGovLogo';
+
+export default {
+  title: 'Components/FHRS/Local Authority',
+};
+
+export const LocalAuthority = (args) => localAuthority(args);
+LocalAuthority.args = {
+  local_authority: 'Local Authority',
+  name_title: 'Name',
+  name_content: 'Coventry City Council',
+  website_title: 'Website',
+  website_url: 'www.coventry.gov.uk',
+  email_title: 'Email',
+  email_address: 'ehcommercial@coventry.gov.uk',
+  logo_svg: logo,
+};
