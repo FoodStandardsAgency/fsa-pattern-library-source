c64338fdfaa5334e9ff2ae9d38d576f575b7241f
Alexey Beloglazov <beloglazov91@gmail.com>
2022-03-31 11:02:04 +0400

    Delete all callbacks because we decided don't use it.

--- src/components/haccp/SaveProgress/saveProgress.stories.js
+++ src/components/haccp/SaveProgress/saveProgress.stories.js
@@ -7,25 +7,11 @@ export default {
 
 const Template = (args) => saveProgress(args);
 
-// The custom callback for a "Previous" button.
-// In Drupal implementation locate it in "Drupal.behaviors" namespace.
-window.customSaveProgressPrevCallback = function () {
-  alert('Here is a previous button');
-};
-
-// The custom callback for a "Save and continue" button.
-// In Drupal implementation locate it in "Drupal.behaviors" namespace.
-window.customSaveProgressNextCallback = function () {
-  alert('Here is a save and continue button');
-};
-
 export const SaveProgress = Template.bind({});
 SaveProgress.args = {
   previous: 'Previous',
   prev_link: '#',
   save_and_continue: 'Save and continue',
-  prev_callback: 'customSaveProgressPrevCallback',
-  next_callback: 'customSaveProgressNextCallback',
   message:
     '<p>Navigating within the tool will save your progress.</p><p>If you are leaving the tool entirely you can <a href="#">save this page</a></p>',
 };
