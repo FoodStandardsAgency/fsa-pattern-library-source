45a628256e781a4a2e6289d6778c9055cb521e8a
Yinuo Meng <mengyn1999@gmail.com>
2022-04-01 11:46:25 +0100

    FFHRS-2 - Increase local authority email address length - Make local authority anchor links wrap when it overflows

--- src/components/fhrs/LocalAuthority/localAuthority.stories.js
+++ src/components/fhrs/LocalAuthority/localAuthority.stories.js
@@ -14,6 +14,6 @@ LocalAuthority.args = {
   website_title: 'Website',
   website_url: 'www.coventry.gov.uk',
   email_title: 'Email',
-  email_address: 'ehcommercial@coventry.gov.uk',
+  email_address: 'ehcommercialverylongemailaddress@coventry.gov.uk',
   logo_svg: logo,
 };
