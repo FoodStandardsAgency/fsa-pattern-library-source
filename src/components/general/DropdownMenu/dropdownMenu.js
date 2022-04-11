877c16fa86347de6c1d186ccf9c8065a14224710
Alexey Beloglazov <beloglazov91@gmail.com>
2022-04-11 11:15:38 +0400

    HACCP-29: MyHACCP Web Tool page.

--- src/components/general/DropdownMenu/dropdownMenu.js
+++ src/components/general/DropdownMenu/dropdownMenu.js
@@ -6,8 +6,14 @@ export default function () {
   function callback() {
     const dropdowns = document.querySelectorAll('.dropdown-menu__label');
 
+    if (!dropdowns.length) {
+      return;
+    }
+
     for (const dropdown of dropdowns) {
       dropdown.addEventListener('click', (e) => {
+        e.preventDefault();
+
         const openedList = document.querySelector('.dropdown-menu__options-open');
         const options = e.target.closest('.dropdown-menu').querySelector('.dropdown-menu__options');
 
@@ -19,16 +25,23 @@ export default function () {
       });
     }
 
-    document.addEventListener('click', (e) => {
-      if (e.target.closest('.dropdown-menu') === null) {
-        const openedList = document.querySelector('.dropdown-menu__options-open');
-
-        if (openedList) {
-          openedList.classList.remove('dropdown-menu__options-open');
-        }
+    document.addEventListener('click', hideUnfocusedDropdowns);
+    document.addEventListener('keyup', (e) => {
+      if (e.key === 'Tab') {
+        hideUnfocusedDropdowns(e);
       }
     });
   }
 
   return domContentLoadedWrapper(callback);
+}
+
+function hideUnfocusedDropdowns(e) {
+  if (e.target.closest('.dropdown-menu') === null) {
+    const openedList = document.querySelector('.dropdown-menu__options-open');
+
+    if (openedList) {
+      openedList.classList.remove('dropdown-menu__options-open');
+    }
+  }
 }
